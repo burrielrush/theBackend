87a996644922f639ab2830266471de05e28c52d6
Rush Burriel <burrielrush@gmail.com>
2023-05-30 21:30:01 -0600

    update

--- db/schema.sql
+++ db/schema.sql
@@ -4,40 +4,40 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
--- SELECT DATABASE
-USE ecommerce_db;
+-- -- SELECT DATABASE
+-- USE ecommerce_db;
 
--- CREATE CATEGORY TABLE
-CREATE TABLE Category (
-  id INT NOT NULL AUTO_INCREMENT,
-  category_name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-);
+-- -- CREATE CATEGORY TABLE
+-- CREATE TABLE Category (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   category_name VARCHAR(255) NOT NULL,
+--   PRIMARY KEY (id)
+-- );
 
--- CREATE THE PRODUCT TABLE
-CREATE TABLE Product (
-  id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(255) NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  stock INT NOT NULL DEFAULT 10,
-  category_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (category_id) REFERENCES Category (id)
-);
+-- -- CREATE THE PRODUCT TABLE
+-- CREATE TABLE Product (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   product_name VARCHAR(255) NOT NULL,
+--   price DECIMAL(10,2) NOT NULL,
+--   stock INT NOT NULL DEFAULT 10,
+--   category_id INT,
+--   PRIMARY KEY (id),
+--   FOREIGN KEY (category_id) REFERENCES Category (id)
+-- );
 
--- CREATE THE TAG TABLE
-CREATE TABLE Tag (
-  id INT NOT NULL AUTO_INCREMENT,
-  tag_name VARCHAR(255),
-  PRIMARY KEY (id)
-);
+-- -- CREATE THE TAG TABLE
+-- CREATE TABLE Tag (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   tag_name VARCHAR(255),
+--   PRIMARY KEY (id)
+-- );
 
--- CREATE THE PRODUCTTAG TABLE
-CREATE TABLE ProductTag (
-  id INT NOT NULL AUTO_INCREMENT,
-  product_id INT,
-  tag_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (product_id) REFERENCES Product (id),
-  FOREIGN KEY (tag_id) REFERENCES Tag (id)
-);
+-- -- CREATE THE PRODUCTTAG TABLE
+-- CREATE TABLE ProductTag (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   product_id INT,
+--   tag_id INT,
+--   PRIMARY KEY (id),
+--   FOREIGN KEY (product_id) REFERENCES Product (id),
+--   FOREIGN KEY (tag_id) REFERENCES Tag (id)
+-- );
